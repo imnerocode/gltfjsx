e43729e982b7c58ce1477253d125bbea8146f56e
imnerocode <jesalemalo@icloud.com>
2024-12-01 22:06:36 -0500

    feat(templateJsx.go)
    Add parsing data to template using the format of the package template/text

--- templates/templateJsx.go
+++ templates/templateJsx.go
@@ -7,19 +7,37 @@ func TemplateJSX() string {
 	export default function Model(){
 		return (
 			<group>
-				{{range .Nodes}}
+				{{range .Meshes}}
 				<mesh
-					rotation=[{{index .Rotation 0}}, {{index .Rotation 1}}, {{index .Rotation 2}}]
-					scale=[{{index .Scale 0}}, {{index .Scale 1}}, {{index .Scale 2}}]
-					position=[{{index .Position 0}}, {{index .Position 1}}, {{index .Position 2}}]
+					rotation={[{{.Node.Rotation 0}}, {{.Node.Rotation 1}}, {{.Node.Rotation 2}}]}
+					scale={[{{.Node.Scale 0}}, {{.Node.Scale 1}}, {{.Node.Scale 2}}]}
+					position={[{{.Node.Position 0}}, {{.Node.Position 1}}, {{.Node.Position 2}}]}
 				>
+					<bufferGeometry>
+						<bufferAttribute
+							attach="attributes-position"
+							array={new Float32Array({{.Attributes.Position}})}
+							count={{len .Attributes.Position}}
+							itemSize={3}
+						/>
+						<bufferAttribute
+							attach="attributes-normal"
+							array={new Float32Array({{.Attributes.Normal}})}
+							count={{len .Attributes.Normal}}
+							itemSize={3}
+						/>
+						<bufferAttribute
+							attach="attributes-uv"
+							array={new Float32Array({{.Attributes.TexCoord}})}
+							count={{len .Attributes.TexCoord}}
+							itemSize={2}
+						/>
+					</bufferGeometry>
 				</mesh>
 				{{end}}
 			</group>
-
 		)
 	}
 	`
-
 	return templateJsx
 }
